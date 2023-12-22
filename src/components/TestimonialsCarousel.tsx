85ca6b57c6fd8cf22215b8093124ab35c3232d36
ChhikaraBRUH <chaitanyachhikara@gmail.com>
2023-12-22 15:58:32 +0530

    fix: add highlighting to testimonials

--- src/components/TestimonialsCarousel.tsx
+++ src/components/TestimonialsCarousel.tsx
@@ -90,7 +90,18 @@ const TESTIMONIALS = [
     imgUrl: "/testimonials/testimonial1.webp",
     heading:
       "Best Document AI solution that comes with pre-trained APIs a CRE lender needs",
-    description: `"Amongst others, the biggest advantage of partnering with Docsumo is the data capture accuracy they’re able to deliver. We’re witnessing a 95%+ STP rate, that means we don’t even have to look at risk assessment documents 95 out of 100 times, and the extracted data is directly pushed into the database."`,
+    description: (
+      <p>
+        &quot;Amongst others, the biggest advantage of partnering with Docsumo
+        is the data capture accuracy they’re able to deliver. We’re witnessing a
+        95%+ STP rate, that means{" "}
+        <span className="bg-brand-blue px-1 text-white">
+          we don’t even have to look at risk assessment documents 95 out of 100
+          times,
+        </span>{" "}
+        and the extracted data is directly pushed into the database.&quot;
+      </p>
+    ),
     name: "Howard Leiner",
     designation: "CTO, Arbor",
     bgColor: "#e0edcd",
@@ -99,7 +110,18 @@ const TESTIMONIALS = [
     imgUrl: "/testimonials/testimonial2.webp",
     heading:
       "Docsumo is your go-to solution if you need a flexible solution to capture data from unstructured documents",
-    description: `“Docsumo does a very good job when it comes to our specific use-case. Debt settlement letters vary a lot from each other, but Docsumo manages to capture data accurately almost every single time at the processing speed which is unprecedented. We’re witnessing the STP rate of over 95% with Docsumo.”`,
+    description: (
+      <p>
+        &quot;Docsumo does a very good job when it comes to our specific
+        use-case. Debt settlement letters vary a lot from each other, but
+        Docsumo manages to capture data accurately almost every single time at
+        the processing speed which is unprecedented.{" "}
+        <span className="bg-brand-blue px-1 text-white">
+          We’re witnessing the STP rate of over 95% with Docsumo.
+        </span>
+        &quot;
+      </p>
+    ),
     name: "Daniel Tilipman",
     designation: "President & Co-Founder, National Debt Relief",
     bgColor: "#cfe8e5",
@@ -108,7 +130,19 @@ const TESTIMONIALS = [
     imgUrl: "/testimonials/testimonial3.webp",
     heading:
       "With Docsumo we are now able to process thousands of ACORD Forms in a day",
-    description: `"We were looking for a tech partner to automate analysis of certificates of insurance for our real estate liability management software. We are really happy with Docsumo’s APIs and their team’s dedication to solving our problem. We are now able to process thousands of ACORD forms a day automatically while being able to get accurate analytics from over 100 data points”`,
+    description: (
+      <p>
+        &quot;We were looking for a tech partner to automate analysis of
+        certificates of insurance for our real estate liability management
+        software.{" "}
+        <span className="bg-brand-blue px-1 text-white">
+          We are really happy with Docsumo’s APIs and their team’s dedication to
+          solving our problem. We are now able to process thousands of ACORD
+        </span>{" "}
+        forms a day automatically while being able to get accurate analytics
+        from over 100 data points&quot;
+      </p>
+    ),
     name: "Michael Rudman",
     designation: "Co-founder & CTO, Jones",
     bgColor: "#e3cdd6",
@@ -117,7 +151,17 @@ const TESTIMONIALS = [
     imgUrl: "/testimonials/testimonial4.webp",
     heading:
       "With Docsumo, we are now able to save more than 500 hours per month.",
-    description: `“With Docsumo, we are now able to assign barcodes in less than 2 mins. The same process used to take us 20 mins previously. We are now saving hundreds of hours a month generating Advanced Shipment Notifications. It has reduced manual errors drastically.”`,
+    description: (
+      <p>
+        <span className="bg-brand-blue px-1 text-white">
+          &quot;With Docsumo, we are now able to assign barcodes in less than 2
+          mins.
+        </span>{" "}
+        The same process used to take us 20 mins previously. We are now saving
+        hundreds of hours a month generating Advanced Shipment Notifications. It
+        has reduced manual errors drastically.&quot;
+      </p>
+    ),
     name: "Neil Lawrence",
     designation: "Business Process Manager, BiagiBros, California",
     bgColor: "#e0cfa8",
@@ -125,7 +169,18 @@ const TESTIMONIALS = [
   {
     imgUrl: "/testimonials/testimonial5.webp",
     heading: "One solution for different utility bill formats",
-    description: `“We’re processing utility bills from 6 different service providers for portfolio management. The challenge was to have just one solution to process all different versions of bills to save us the hassle of retraining & switching amongst multiple solutions. Docsumo has been able to deliver just that - one solution for all different variations.“`,
+    description: (
+      <p>
+        &quot; We’re processing utility bills from 6 different service providers
+        for portfolio management. The challenge was to have just{" "}
+        <span className="bg-brand-blue px-1 text-white">
+          one solution to process all different versions of bills to save us the
+          hassle of retraining & switching amongst multiple solutions.
+        </span>{" "}
+        Docsumo has been able to deliver just that - one solution for all
+        different variations. &quot;
+      </p>
+    ),
     name: "Shani Nowlin",
     designation: "Chief Technology Officer Westland Real Estate Group",
     bgColor: "#b0d2d8",
@@ -133,7 +188,16 @@ const TESTIMONIALS = [
   {
     imgUrl: "/testimonials/testimonial6.webp",
     heading: "Best in class for capturing data from financial documents",
-    description: `“We are using Docsumo’s APIs for automating data capture from bank statements and identity cards while on-boarding customers. It has reduced the time our operations team spends on data entry by manifolds while providing a much better customer experience.”`,
+    description: (
+      <p>
+        &quot;We are using Docsumo’s APIs for automating data capture from bank
+        statements and identity cards while on-boarding customers.{" "}
+        <span className="bg-brand-blue px-1 text-white">
+          It has reduced the time our operations team spends on data entry by
+          manifolds while providing a much better customer experience.&quot;
+        </span>
+      </p>
+    ),
     name: "Prashanth Ranganathan",
     designation: "CEO, PayU Credit",
     bgColor: "#f8ca98",
